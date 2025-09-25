ec5fab611078673c994bb024e3d1950ce56f42a0
DeveloperUchida <kouei2257@gmail.com>
2025-09-25 16:20:23 +0900

    プログラムアップデート

---
+++ Assets/Scripts/MainScripts.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class MainScripts : MonoBehaviour
+{
+    public GameObject characters; //キャラクター設定
+    bool isGene = false; //キャラクター生成のフラグ
+    GameObject geneChara; //キャラクター生成の感覚調整用
+     
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
